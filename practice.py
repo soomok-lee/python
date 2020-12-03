01ee2db1b28b91dca0a1abfa262714b811312049
soomok-lee <soomok.us@gmail.com>
2020-12-03 13:07:14 -0800

    first chapter.

---
+++ practice.py
@@ -0,0 +1,43 @@
+# data type
+
+# number
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+# string
+print('apple')
+print("banana")
+print("lol")
+print("lol"*9)
+# boolean
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5>10))
+# variable
+animal = "puppy"
+name = "Ella"
+age = 4
+hobby = "walking"
+is_adult = age >= 3
+print("my " + animal + "'s name is " + name)
+print(name + " is " + str(age) + " years old")
+print(name,"is",age,"years old")
+hobby = "running"
+print(name + "'s hobby is " + hobby)
+print("is " + name + " an adult? " + str(is_adult))
+
+# comment
+# ctrl + '/'
+'''comments'''
+
+# quiz 1
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
